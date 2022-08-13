e2e5a9d2fa91d03746f7d150e2c6c0c1c2e01cc0
StefanYosifov <stefyosifov@gmail.com>
2022-08-13 15:31:54 +0300

    Fixing some unit tests

--- 18.Exercise Unit Testing/Skeleton/Database.Tests/DatabaseTests.cs
+++ 18.Exercise Unit Testing/Skeleton/Database.Tests/DatabaseTests.cs
@@ -1,9 +1,9 @@
 namespace Database.Tests
 {
-    using NUnit.Framework;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
+
+    using NUnit.Framework;
 
     [TestFixture]
     public class DatabaseTests
@@ -13,150 +13,188 @@ namespace Database.Tests
         [SetUp]
         public void SetUp()
         {
-            db = new Database();
+            //This will be run before every test
+            this.db = new Database();
         }
 
-
-        public void Test_Constructor()
-        {
-
-        }
-
-
+        //Valid functionality of the ctor
+        //1 -> edge case (no data); 2,3 -> Valid data (success), 4 -> edge case (max data)
+        //This test is meant to test the constructor and nothing more! Single responsibility!
+        //I assume the Fetch() method and the Count getter to be working fine
         [TestCase(new int[] { })]
         [TestCase(new int[] { 1 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 1, 2, 3 })]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 })]
-        public void Constructor_Less_Than_16_Elements(int[] elemnetsToAdd)
+        public void ConstructorShouldAddLessThan16Elements(int[] elementsToAdd)
         {
-            Database database = new Database(elemnetsToAdd);
-            int[] actualData = database.Fetch();
-            int actualCount = actualData.Length;
+            //Arrange
+            Database testDb = new Database(elementsToAdd);
 
-            int expectedCount = elemnetsToAdd.Length;
+            //Act
+            int[] actualData = testDb.Fetch();
+            int[] expectedData = elementsToAdd;
 
-            CollectionAssert.AreEqual(actualData, elemnetsToAdd);
-            Assert.AreEqual(expectedCount, actualCount);
+            int actualCount = testDb.Count;
+            int expectedCount = expectedData.Length;
 
+            //Assert
+            CollectionAssert.AreEqual(expectedData, actualData,
+                "Database constructor should initialize data field correctly!");
+            Assert.AreEqual(expectedCount, actualCount,
+                "Constructor should set initial value for count field!");
         }
 
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 })]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 })]
-        public void Constructor_Must_Not_Allow_More_Than_16_Elements(int[] elemnetsToAdd)
+        public void ConstructorMustNotAllowToExceedMaximumCount(int[] elementsToAdd)
         {
             Assert.Throws<InvalidOperationException>(() =>
             {
-                Database database = new Database(elemnetsToAdd);
-            });
+                Database testDb = new Database(elementsToAdd);
+            }, "Array's capacity must be exactly 16 integers!");
+        }
 
+        //Valid functionality of count
+        //I assume that the ctor is working fine (I tested it!)
+        [Test]
+        public void CountMustReturnActualCount()
+        {
+            int[] initData = new int[] { 1, 2, 3 };
+            Database testDb = new Database(initData);
+
+            int actualCount = testDb.Count;
+            int expectedCount = initData.Length;
+
+            Assert.AreEqual(expectedCount, actualCount,
+                "Count should return the count of the added elements!");
         }
 
         [Test]
-        public void Testing_Add_Functionality(int[] existingArray, int[] elementsToAdd)
+        public void CountMustReturnZeroWhenNoElements()
         {
-            int[] testData = new int[] { 1, 2, 3 };
+            int actualCount = this.db.Count;
+            int expectedCount = 0;
 
-            foreach (var element in elementsToAdd)
+            Assert.AreEqual(expectedCount, actualCount,
+                "Count should be zero when there are no elements in the Database!");
+        }
+
+        [TestCase(new int[] { 1 })]
+        [TestCase(new int[] { 1, 2, 3 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 })]
+        public void AddShouldAddLessThan16Elements(int[] elementsToAdd)
+        {
+            //Act
+            foreach (int el in elementsToAdd)
             {
-                this.db.Add(element);
+                this.db.Add(el);
             }
 
             int[] actualData = this.db.Fetch();
-            int actualDataCount = this.db.Count;
+            int[] expectedData = elementsToAdd;
 
-            Assert.AreEqual(actualDataCount, existingArray.Length);
+            int actualCount = this.db.Count;
+            int expectedCount = expectedData.Length;
 
+            CollectionAssert.AreEqual(expectedData, actualData,
+                "Add should physically add the elements to the field!");
+            Assert.AreEqual(expectedCount, actualCount,
+                "Add should change the elements count when adding!");
         }
 
         [Test]
-        public void Testing_Add_Over_17_Elements()
+        public void AddShouldThrowExceptionWhenAddingMoreThan16Elements()
         {
-            for (int i = 0; i < 16; i++)
+            //Act
+            for (int i = 1; i <= 16; i++)
             {
                 this.db.Add(i);
             }
 
             Assert.Throws<InvalidOperationException>(() =>
             {
-                this.db.Add(1);
-            });
+                this.db.Add(17);
+            }, "Array's capacity must be exactly 16 integers!");
         }
 
-        [TestCase(new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
         [TestCase(new int[] { 1 })]
-        public void RemoveShouldRemoveTheLastElement(int[] startElements)
+        public void RemoveShouldRemoveTheLastElementSuccessfullyOnce(int[] startElements)
         {
-            foreach (var element in startElements)
+            //Act
+            foreach (int el in startElements)
             {
-                this.db.Add(element);
+                this.db.Add(el);
             }
-            this.db.Remove();
 
-            List<int> elementList = new List<int>(startElements);
+            this.db.Remove();
+            List<int> elList = new List<int>(startElements);
+            elList.RemoveAt(elList.Count - 1);
 
             int[] actualData = this.db.Fetch();
-            elementList.RemoveAt(elementList.Count - 1);
-            int[] expectedData = startElements.ToArray();
+            int[] expectedData = elList.ToArray();
 
             int actualCount = this.db.Count;
-            int expectedCount = elementList.Count;
+            int expectedCount = expectedData.Length;
 
-            CollectionAssert.AreEqual(expectedData, actualData);
-            Assert.That(actualCount.Equals(expectedCount));
-
-
-
+            CollectionAssert.AreEqual(expectedData, actualData,
+                "Remove should physically remove the element in the data field!");
+            Assert.AreEqual(expectedCount, actualCount,
+                "Remove should decrement the count of the Database!");
         }
 
         [Test]
         public void RemoveShouldRemoveTheLastElementMoreThanOnce()
         {
-            List<int> testData = new List<int>() { 1, 2, 3, 4, 5 };
-            foreach (var element in testData)
+            List<int> initData = new List<int>() { 1, 2, 3 };
+            foreach (int el in initData)
             {
-                this.db.Add(element);
+                this.db.Add(el);
             }
 
-            for (int i = 0; i < testData.Count; i++)
+            for (int i = 0; i < initData.Count; i++)
             {
                 this.db.Remove();
             }
 
+            int[] actualData = this.db.Fetch();
+            int[] expectedData = new int[] { };
 
+            int actualCount = this.db.Count;
+            int expectedCount = 0;
+
+            CollectionAssert.AreEqual(expectedData, actualData,
+                "Remove should physically remove the element in the data field!");
+            Assert.AreEqual(expectedCount, actualCount,
+                "Remove should decrement the count of the Database!");
         }
 
         [Test]
-        public void RemoveShouldThrowExceptionIf0Elements()
+        public void RemoveShouldThrowErrorWhenThereAreNoElements()
         {
-            List<int> testData = new List<int> { };
-            foreach (var element in testData)
-            {
-                this.db.Add(element);
-            }
-
             Assert.Throws<InvalidOperationException>(() =>
             {
                 this.db.Remove();
-
-            });
-
+            }, "The collection is empty!");
         }
 
-
-        [TestCase(new int[]{ })]
-        [TestCase(new int[] {1,2,3,4,5,6,7,})]
+        [TestCase(new int[] { })]
+        [TestCase(new int[] { 1 })]
+        [TestCase(new int[] { 1, 2, 3 })]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 })]
-
-        public void FetchShoudlReturnACopy(int[] startElements)
+        public void FetchShouldReturnCopyArray(int[] initData)
         {
-            foreach(var element in startElements)
+            //Act
+            foreach (int el in initData)
             {
-                db.Add(element);
+                this.db.Add(el);
             }
+
             int[] actualResult = this.db.Fetch();
-            int[] expectedResult = startElements;
+            int[] expectedResult = initData;
 
-            CollectionAssert.AreEqual(expectedResult, actualResult);
-
+            CollectionAssert.AreEqual(expectedResult, actualResult,
+                "Fetch should return copy of the existing data!");
         }
     }
 }
