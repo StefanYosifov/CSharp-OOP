1678f866f10f81e230f58e450a8b16b8c2d96cc5
StefanYosifov <stefyosifov@gmail.com>
2022-10-15 21:31:18 +0300

    The real exam

---
+++ New Exam/OOP/Models/Rooms/DoubleBed.cs
@@ -0,0 +1,15 @@
+﻿namespace BookingApp.Models.Rooms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DoubleBed : Room
+    {
+        private const int doubleBedCapacity = 2;
+
+        public DoubleBed() : base(doubleBedCapacity)
+        {
+        }
+    }
+}
---
+++ New Exam/OOP/Repositories/RoomRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace BookingApp.Repositories
+{
+    using BookingApp.Models.Rooms.Contracts;
+    using BookingApp.Repositories.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RoomRepository : IRepository<IRoom>
+    {
+        private List<IRoom> models;
+
+        public RoomRepository()
+        {
+            this.models=new List<IRoom>();
+        }
+
+       
+
+        public void AddNew(IRoom model)
+        {
+            this.models.Add(model);
+        }
+
+        public IReadOnlyCollection<IRoom> All()
+        {
+            return models as IReadOnlyCollection<IRoom>;
+        }
+
+        public IRoom Select(string criteria)
+        {
+            var room = models.FirstOrDefault(x =>x.GetType().Name == criteria);
+            return room;
+        }
+    }
+}
